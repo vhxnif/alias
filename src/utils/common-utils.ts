e9096c1924b4aa771217ec5bef624ba34aa71068
vhxnif <chen517000@live.com>
2024-12-13 23:16:43 +0800

    feat: add ChalkInstance type and refactor mark functions to use colorMark utility

--- src/utils/common-utils.ts
+++ src/utils/common-utils.ts
@@ -1,6 +1,7 @@
 import { select } from "@inquirer/prompts"
 import { table, type TableUserConfig } from "table"
 import { color, style } from "./color-utils"
+import type { ChalkInstance } from "chalk"
 
 const terminal: Record<string, number> = {
     'column': process.stdout.columns,
@@ -49,13 +50,14 @@ const tableConfig = (cols: number[], maxCol: number = 70): TableUserConfig => {
 const printTable = (data: unknown[][], userConfig?: TableUserConfig) => console.log(table(data, userConfig))
 const selectRun = async (message: string, choices: { name: string, value: string }[], f: (str: string) => void) => f(await select({ message, choices }))
 const printErr = (str: string) => console.log(color.red(str))
+const colorMark = (str: string, reg: RegExp, color: ChalkInstance) => str.replace(reg, match => color(match))
 const mark = {
-    singleQuotationMark: (str: string) => str.replace(/'([^']+)'/g, match => color.green(match)),
-    doubleQuotationMark: (str: string) => str.replace(/"([^"]+)"/g, match => color.maroon(match)),
-    pathMark: (str: string) => str.replace(/(\.\.\/)?([\w-]+\/)*[\w-]+\.\w+/g, match => color.mauve(match)),
-    numberMak: (str: string) => str.replace(/\s(\d+)\s/g, match => color.sky(match)),
-    doublePlusMark: (str: string) => str.replace(/\++/g, match => color.green(match)),
-    minusPlusMark: (str: string) => str.replace(/-+/g, match => color.red(match))
+    singleQuotationMark: (str: string) => colorMark(str, /'([^']+)'/g, color.green),
+    doubleQuotationMark: (str: string) => colorMark(str, /"([^"]+)"/g, color.maroon),
+    pathMark: (str: string) => colorMark(str, /(\.\.\/)?([\w-]+\/)*[\w-]+\.\w+/g, color.mauve),
+    numberMak: (str: string) => colorMark(str, /\s(\d+)\s/g, color.sky),
+    doublePlusMark: (str: string) => colorMark(str, /\++/g, color.green),
+    minusPlusMark: (str: string) => colorMark(str, /-+/g, color.red)
 }
 
 const printCmdLog = (str: string) => {
