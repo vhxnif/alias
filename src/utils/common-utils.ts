6f9aa8a94772d81c07d9b0296aad2253c14ff264
xiaochen <chen517000@live.com>
2024-12-13 23:06:10 +0800

    refactor: improve printCmdLog function and extract color marking logic

--- src/utils/common-utils.ts
+++ src/utils/common-utils.ts
@@ -49,18 +49,24 @@ const tableConfig = (cols: number[], maxCol: number = 70): TableUserConfig => {
 const printTable = (data: unknown[][], userConfig?: TableUserConfig) => console.log(table(data, userConfig))
 const selectRun = async (message: string, choices: { name: string, value: string }[], f: (str: string) => void) => f(await select({ message, choices }))
 const printErr = (str: string) => console.log(color.red(str))
-const printCmdLog = (str: string) => console.log(
-    str.replace(/'([^']+)'/g, match => color.green(match))
-        .replace(/"([^"]+)"/g, match => color.maroon(match))
-        .replace(/(\.\.\/)?([\w-]+\/)*[\w-]+\.\w+/g, match => color.mauve(match))
-        .replace(/\++/g, match => color.red(match))
-        .replace(/-+/g, match => color.green(match))
-        .replace(/\s(\d+)\s/g, match => color.sky(match))
-)
+const mark = {
+    singleQuotationMark: (str: string) => str.replace(/'([^']+)'/g, match => color.green(match)),
+    doubleQuotationMark: (str: string) => str.replace(/"([^"]+)"/g, match => color.maroon(match)),
+    pathMark: (str: string) => str.replace(/(\.\.\/)?([\w-]+\/)*[\w-]+\.\w+/g, match => color.mauve(match)),
+    numberMak: (str: string) => str.replace(/\s(\d+)\s/g, match => color.sky(match)),
+    doublePlusMark: (str: string) => str.replace(/\++/g, match => color.green(match)),
+    minusPlusMark: (str: string) => str.replace(/-+/g, match => color.red(match))
+}
+
+const printCmdLog = (str: string) => {
+    let tmp = str
+    Object.values(mark).forEach(it => { tmp = it(tmp) })
+    console.log(tmp)
+}
 
 const oraText = (str: string) => style.bold(str)
 
 const title = (strs: string[]) => strs.map(str => color.green(style.bold(str)))
 
-export { printCmdLog, printErr, printTable, selectRun, tableConfig, oraText, title, tableDefaultConfig, terminal }
+export { printCmdLog, printErr, printTable, selectRun, tableConfig, oraText, title, tableDefaultConfig, terminal, mark }
 
