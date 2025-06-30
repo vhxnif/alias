8e636b7b6283086d30fb1bce8e83733223243ddd
vhxnif <chen517000@live.com>
2025-06-30 17:12:35 +0800

    refactor: simplify errParse function by removing unnecessary wrapper and adding direct error handling

--- src/utils/command-utils.ts
+++ src/utils/command-utils.ts
@@ -1,11 +1,11 @@
 import { printErr } from "./common-utils"
 
-async function errParse() {
-  return (e: unknown) => {
-    if (e instanceof Error) {
-      printErr(e.message)
-    }
+async function errParse(e: unknown) {
+  if (e instanceof Error) {
+    printErr(e.message)
+    return
   }
+  console.log(e)
 }
 
 export { errParse }
